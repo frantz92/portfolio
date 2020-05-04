482924c3934e2bc2573b3e412be3a277dde63b22
Patryk Frankowski <patryk.frankowski@o2.pl>
2020-05-04 13:04:34 +0200

    Add transitions for title & name & nav & next div

---
+++ js/script.js
@@ -0,0 +1,44 @@
+let scroll =
+  window.requestAnimationFrame ||
+  function (callback) {
+    window.setTimeout(callback, 1000 / 60);
+  };
+
+let elementsToShow = document.querySelectorAll('.show-on-scroll');
+
+window.addEventListener('scroll', function (element) {
+  console.log('scroll');
+  let aboutToShow = document.querySelector('.about');
+  aboutToShow.classList.add('is-visible');
+});
+
+function loop() {
+  elementsToShow.forEach(function (element) {
+    if (isElementInViewport(element)) {
+      element.classList.add('is-visible');
+    } else {
+      element.classList.remove('is-visible');
+    }
+  });
+  scroll(loop);
+}
+
+loop();
+
+function isElementInViewport(el) {
+  // special bonus for those using jQuery
+  if (typeof jQuery === 'function' && el instanceof jQuery) {
+    el = el[0];
+  }
+  var rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0 && rect.bottom >= 0) ||
+    (rect.bottom >=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <=
+        (window.innerHeight || document.documentElement.clientHeight)) ||
+    (rect.top >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
