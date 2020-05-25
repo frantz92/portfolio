270c1167534be98aee2dd99f4a9a80a7baa88aa2
Patryk Frankowski <patryk.frankowski@o2.pl>
2020-05-25 16:02:53 +0200

    Add contact section, with buttons, add eye effect

--- js/script.js
+++ js/script.js
@@ -37,9 +37,33 @@ function isElementInViewport(el) {
     (rect.bottom >=
       (window.innerHeight || document.documentElement.clientHeight) &&
       rect.top <=
-        (window.innerHeight || document.documentElement.clientHeight)) ||
+      (window.innerHeight || document.documentElement.clientHeight)) ||
     (rect.top >= 0 &&
       rect.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight))
+      (window.innerHeight || document.documentElement.clientHeight))
   );
+}
+
+let eyeball = document.querySelector('.eye');
+let eye = document.querySelector('.front_end_button')
+document.onmousemove = function () {
+
+  let rect = eyeball.getBoundingClientRect();
+
+  let xCenter = (rect.right + rect.left) / 2;
+  let yCenter = (rect.top + rect.bottom) / 2;
+
+  console.log("left:", rect.left, "right:", rect.right, "center:", xCenter)
+
+  let x = event.clientX * 50 / xCenter + '%';
+  let y = event.clientY * 50 / yCenter + '%';
+
+  if (y > yCenter) {
+    eyeball.style.top = 0 + '%';
+  }
+  else {
+    eye.style.top = y;
+  }
+
+  eye.style.left = x;
 }
