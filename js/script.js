fd308ed95df62aa026b9618e952b64ee7bfc571a
Patryk Frankowski <patryk.frankowski@o2.pl>
2020-11-05 13:41:59 +0100

    Remove previous horizontal scroll function, add new in JS

--- js/script.js
+++ js/script.js
@@ -31,7 +31,6 @@ function loop() {
 loop();
 
 function isElementInViewport(el) {
-  // special bonus for those using jQuery
   if (typeof jQuery === 'function' && el instanceof jQuery) {
     el = el[0];
   }
@@ -47,29 +46,29 @@ function isElementInViewport(el) {
       (window.innerHeight || document.documentElement.clientHeight))
   );
 }
-
-let eyeball = document.querySelector('.eye');
-let eye = document.querySelector('.front_end_button')
-document.onmousemove = function () {
-
-  let rect = eyeball.getBoundingClientRect();
-
-  let xCenter = (rect.right + rect.left) / 2;
-  let yCenter = (rect.top + rect.bottom) / 2;
-
-  let x = event.clientX * 50 / xCenter + '%';
-  let y = event.clientY * 50 / yCenter + '%';
-
-  if (y > yCenter) {
-    eyeball.style.top = 0 + '%';
+horizontalWrappers();
+/* Horizontal scroll */
+function horizontalWrappers() {
+  const horizontalWrappers = document.querySelectorAll('.wrapper-outside');
+  for(const horizontalWrapper of horizontalWrappers) {
+    //let maxScrollLeft = horizontalWrapper.scrollWidth - horizontalWrapper.clientWidth;
+    const innerWrapper = document.querySelector('.wrapper');
+    if(innerWrapper.getBoundingClientRect().right <= innerWrapper.offsetWidth && innerWrapper.getBoundingClientRect().right >= horizontalWrapper.offsetWidth) {
+      horizontalWrapper.addEventListener('wheel', (e) => {
+        horizontalWrapper.scrollLeft += e.deltaY;
+        console.log('outer', horizontalWrapper.offsetWidth);
+        console.log('inner', innerWrapper.offsetWidth);
+        console.log(innerWrapper.getBoundingClientRect().right);
+        //console.log('elo');
+        e.preventDefault();
+      })
+    } else {
+      console.log('Siemanko!');
+      return;
+    }
   }
-  else {
-    eye.style.top = y;
-  }
-
-  eye.style.left = x;
 }
-
+/* Add scrolling info */
 const firstCircle = document.querySelector('.circle');
 firstCircle.onmouseover = function() {
   const checkEducation = document.querySelector('.check-education');
@@ -77,9 +76,26 @@ firstCircle.onmouseover = function() {
   const scrollDown = document.querySelector('.scroll-down');
   scrollDown.classList.add('is-visible');
 }
-
+/* Remove scrolling info */
 const educationWrapper = document.querySelector('.wrapper-outside');
 educationWrapper.onscroll = function() {
   const scrollDown = document.querySelector('.scroll-down');
   scrollDown.classList.remove('is-visible');
+}
+/* Eye movement */
+const eyeball = document.querySelector('.eye');
+const eye = document.querySelector('.front_end_button')
+document.onmousemove = function () {
+  let rect = eyeball.getBoundingClientRect();
+  let xCenter = (rect.right + rect.left) / 2;
+  let yCenter = (rect.top + rect.bottom) / 2;
+  let x = event.clientX * 50 / xCenter + '%';
+  let y = event.clientY * 50 / yCenter + '%';
+  if (y > yCenter) {
+    eyeball.style.top = 0 + '%';
+  }
+  else {
+    eye.style.top = y;
+  }
+  eye.style.left = x;
 }
